8a9513659a41eac76b4ef0590441e2adaca8c825
g33k_freak <zaripo.rus@gmail.com>
2020-06-07 15:54:53 +0200

    Created adding to the cart service based on AJAX request

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -7,5 +7,33 @@ for (i = 0; i < addBtns.length; i++) {
         console.log('productId: ', productId, 'action: ', action)
 
         console.log('User: ', user)
+
+        if (user === "AnonymousUser") {
+            console.log("Not logged in")
+        } else {
+            updateUserOrder(productId, action)
+        }
     })
+}
+
+function updateUserOrder(productId, action) {
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+
+        .then((response) => {
+            return response.json()
+        })
+
+        .then((data) => {
+            console.log('data:', data)
+            location.reload()
+        })
 }
